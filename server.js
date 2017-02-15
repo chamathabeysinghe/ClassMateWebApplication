4892c2446ecd5092ada34c13b10f0d2054961b61
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2017-02-15 23:15:55 +0530

    add template for the client

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var port = 3000;
 var app=express();
 
 //view engine
-app.set('views',path.join(__dirname,'views'));
+app.set('views',path.join(__dirname,'client/views'));
 app.set('view engine','ejs');
 app.engine('html',require('ejs').renderFile);
 
