3c810886ea426d2dcd0bfb49a42a0b69aa1b9592
Chamath Abeysinghe <abeysinghechamath@gmail.com>
2017-05-10 14:51:24 +0530

    Change the User model

--- routes/payement.js
+++ routes/payement.js
@@ -4,18 +4,30 @@ var router=express.Router();
 var jwt         = require('jwt-simple');
 var config      = require('../config/database'); // get db config file
 var passport	= require('passport');
-var getToken=require('../commons/utilities');
 
 var stripe=require("stripe")("sk_test_mYlfi9fIY8XupeuOvpk6abcV");
 
-router.post('/pay',function (req, res) {
+function getToken(headers) {
 
-    // var userToken = getToken(req.headers);
-    // var user = jwt.decode(userToken, config.secret);
-    //
-    // if (user.accountType=='student') {
-    //     return res.json({success:false,"error":"invalid user type"});
-    // }
+        var parted = headers.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+
+};
+
+
+
+router.post('/pay',function (req, res) {
+    console.log(req.body.id_token);
+    var userToken = getToken(req.body.id_token);
+    var user = jwt.decode(userToken, config.secret);
+    console.log(user.firstName);
+    if (user.accountType=='student') {
+        return res.json({success:false,"error":"invalid user type"});
+    }
 
     var token=req.body.stripeToken;
     var chargeAmount=req.body.chargeAmount;
@@ -23,16 +35,13 @@ router.post('/pay',function (req, res) {
     console.log(chargeAmount);
     var charge=stripe.charges.create({
         amount:chargeAmount,
-        currency:"gbp",
+        currency:"usd",
         source:token
     },function (err, charge) {
         if(err){
             console.log("Your card was declined");
         }
-        console.log(err);
-        console.log("**********************Success***************"+charge)
     });
-    console.log("Payment success");
     res.redirect("http://localhost:4200/#/dashboard");
 
 });
